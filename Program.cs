7cde990ea6a472706a4bb9447512657b002d9d52
wisoums <wissem_oumsalem@yahoo.ca>
2024-05-18 17:04:34 -0400

    oui

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace StopWatch;
+
+public class Program
+{
+    public static void displayMenu()
+    {
+        Console.WriteLine("****************************" +
+                          "\n1- Start the chronometer" +
+                          "\n2- Stop the chronometer" +
+                          "\n3- Quit" +
+                          "\n****************************");
+    }
+
+    public static int takeInputChoice()
+    {
+        Console.Write("Enter a choice: ");
+        int choice;
+        while (!int.TryParse(Console.ReadLine(), out choice) || !isValidChoice(choice))
+        {
+            Console.Write("Your choice is invalid. Try again: ");
+        }
+        return choice;
+    }
+    
+    public static Boolean isValidChoice(int choice)
+    {
+        return (choice <= 3 && choice >= 1);
+    }
+    
+    public static void Main(string[] args)
+    {
+        var sw1 = new Stopwatch();
+        
+        TimeSpan duration;
+        int choice;
+        do
+        {
+            displayMenu();
+            choice = takeInputChoice();
+            switch (choice)
+            {
+                case 1 :
+                    sw1.start();
+                    Console.WriteLine("Your StopWatch successfully started.");
+                    break; 
+                case 2 :
+                    sw1.stop();
+                    break;
+            }
+        } while (choice != 3);
+        Console.WriteLine("Thank you for using StopWatchPro");
+    }
+}
+
--- Stopwatch.cs
+++ Stopwatch.cs
@@ -2,5 +2,68 @@ namespace StopWatch;
 
 public class Stopwatch
 {
+    private DateTime _startTime;
+    private DateTime _stopTime;
+    private Boolean _isRunning;
+    private TimeSpan _duration;
     
+
+    public DateTime StartTime
+    {
+        get => _startTime;
+        set => _startTime = value;
+    }
+
+    public DateTime StopTime
+    {
+        get => _stopTime;
+        set => _stopTime = value;
+    }
+    
+    public Boolean IsRunning
+    {
+        get => _isRunning;
+        set => _isRunning = value;
+    }
+    public TimeSpan Duration
+    {
+        get => _duration;
+        set => _duration = value;
+    }
+
+    public Stopwatch()
+    {
+        _startTime = DateTime.MinValue;
+        _duration = TimeSpan.Zero;
+        _isRunning = false;
+    }
+
+    public void start()
+    {
+        if (_isRunning)
+        {
+            Console.WriteLine("This chronometer is already running. Try stopping it.");
+        }
+        else
+        {
+            _startTime = DateTime.Now;
+            _isRunning = true;
+        }
+        
+    }
+
+    public void stop()
+    {
+        if (!_isRunning)
+        {
+            Console.WriteLine("This chronometer never started running. Try starting it before.");
+        }
+        else
+        {
+            _stopTime = DateTime.Now;
+            _duration = _stopTime - _startTime;
+            Console.WriteLine("Your StopWatch successfully stopped. \nTime: "+ _duration);
+            _isRunning = false;
+        }
+    }
 }
